77323b3bab2fb83d66868f1ff4d2170733d72c63
LCLY <lchoo9294@gmail.com>
2020-10-10 01:56:08 +0800

    feat: integrated typescript

---
+++ src/components/Navigation/CustomBurger/CustomBurger.tsx
@@ -0,0 +1,31 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import './CustomBurger.scss';
+
+type CustomBurgerProps = {
+    showElement: boolean, 
+    setShowElement: Dispatch<SetStateAction<boolean>>   
+};
+
+function CustomBurger({showElement, setShowElement}:CustomBurgerProps) {
+    
+    return (
+        <>
+            <input
+                type="checkbox"
+                id="customburger__toggle"
+                checked={showElement}
+                className="customburger__bars-checkbox"
+                readOnly
+            />
+            <label
+                htmlFor="customburger__toggle"
+                className="customburger__bars-button customburger__bars-div"
+                onClick={() => setShowElement(!showElement)}
+            >
+                <div className="customburger__bars-icon">&nbsp;</div>
+            </label>
+        </>
+    );
+}
+
+export default CustomBurger;
